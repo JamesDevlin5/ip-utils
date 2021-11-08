500f89e59b9a2dcda3195f0923b913a69354d2e1
James Devlin <devlin.ja@northeastern.edu>
2021-11-08 12:55:53 -0500

    Added Network Manipulation
    Created a means of traversing a network hierarchy. A network's subnets
    or supernets may be retrieved, if they exist.

--- src/net.rs
+++ src/net.rs
@@ -40,6 +40,30 @@ impl IpNetwork {
     pub fn num_host_bits(&self) -> u8 {
         32 - self.num_network_bits()
     }
+
+    /// The supernet of some IP network is one bit less-specific than its subnets. This means that
+    /// the address space is one bit more ambiguous, and offers a power of two more addresses
+    /// within the network set.
+    pub fn supernet(self) -> Option<Self> {
+        match self.num_network_bits() {
+            0 => None,
+            n => Self::new(self.base, n - 1),
+        }
+    }
+
+    /// Gets the two children of this network. The point of contention for these two networks will
+    /// be the immediate new bit in the prefix. This bit may be a `1` or a `0`, where before this
+    /// number was irrelevant. Each of these children networks will contain exactly half of the
+    /// supernet.
+    pub fn subnets(self) -> Option<(Self, Self)> {
+        if let Some(lower_net) = Self::new(self.base, self.num_network_bits() + 1) {
+            let mut upper_net = lower_net.clone();
+            upper_net.base = (*lower_net.base | (1 << lower_net.num_host_bits())).into();
+            Some((upper_net, lower_net))
+        } else {
+            None
+        }
+    }
 }
 
 impl fmt::Display for IpNetwork {
@@ -74,5 +98,48 @@ mod tests {
         assert!(IpNetwork::new(IpAddress::from(0), 33).is_none());
         assert!(IpNetwork::new(IpAddress::from(0), 200).is_none());
         assert!(IpNetwork::new(IpAddress::from(0), 255).is_none());
+    }
+
+    #[test]
+    fn supernet() {
+        assert!(IpNetwork::new(IpAddress::from(0), 0)
+            .unwrap()
+            .supernet()
+            .is_none());
+        assert_eq!(
+            IpNetwork::new(IpAddress::from(0), 1)
+                .unwrap()
+                .supernet()
+                .unwrap(),
+            IpNetwork::new(IpAddress::from(0), 0).unwrap()
+        );
+        assert_eq!(
+            IpNetwork::new(IpAddress::from(4290772992), 10)
+                .unwrap()
+                .supernet()
+                .unwrap(),
+            IpNetwork::new(IpAddress::from(4290772992), 9).unwrap()
+        );
+        assert_eq!(
+            IpNetwork::new(IpAddress::from(4292870144), 14)
+                .unwrap()
+                .supernet()
+                .unwrap(),
+            IpNetwork::new(IpAddress::from(4292870144), 13).unwrap()
+        );
+        assert_eq!(
+            IpNetwork::new(IpAddress::from(3578789888), 30)
+                .unwrap()
+                .supernet()
+                .unwrap(),
+            IpNetwork::new(IpAddress::from(3578789888), 29).unwrap()
+        );
+        assert_eq!(
+            IpNetwork::new(IpAddress::from(u32::MAX - 3), 32)
+                .unwrap()
+                .supernet()
+                .unwrap(),
+            IpNetwork::new(IpAddress::from(u32::MAX - 3), 31).unwrap()
+        );
     }
 }
